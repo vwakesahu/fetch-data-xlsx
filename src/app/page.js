f31ae954fb54fe11256f01365824968af157f9f4
Vivek Sahu <viveksahu1762@gmail.com>
2024-06-12 12:57:08 +0530

    Supports Bulk Uploading

--- src/app/page.js
+++ src/app/page.js
@@ -1,277 +1,72 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-  SelectItem,
-} from "@/components/ui/select";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedValue, setEditedValue] = useState("");
-  const [editedCell, setEditedCell] = useState({ rowIndex: 0, columnName: "" });
-  const [data, setData] = useState([]);
-  const [columnMappings, setColumnMappings] = useState({});
-  const [mappedData, setMappedData] = useState([]);
-  const [newFixedColumn, setNewFixedColumn] = useState("");
-  const [fixedColumns, setFixedColumns] = useState([
-    "First Name",
-    "Country",
-    "Date of Birth",
-    "Grade",
-    "Email",
-    "Password",
-    "Address",
-    "Stop Name",
-  ]);
-  const [selectedRowIndex, setSelectedRowIndex] = useState(0);
-
-  useEffect(() => {
-    // Load data when selectedRowIndex changes
-    if (mappedData.length > 0) {
-      const selectedRow = mappedData[selectedRowIndex];
-      console.log("Selected Row Data:", selectedRow);
-      // Perform additional actions or data loading based on the selected row
-    }
-  }, [selectedRowIndex, mappedData]);
-
-  const handleCellClick = (rowIndex, columnName, value) => {
-    setEditedCell({ rowIndex, columnName });
-    setEditedValue(value);
-    setIsEditing(true);
-  };
-
-  const handleInputBlur = () => {
-    setIsEditing(false);
-
-    // Update the data with the edited value
-    if (editedCell.columnName && editedValue !== "") {
-      const updatedValue = convertValueToColumnType(
-        editedValue,
-        editedCell.columnName
-      );
-      handleInputValueChange(
-        editedCell.rowIndex,
-        editedCell.columnName,
-        updatedValue
-      );
-    }
-  };
-
-  const convertValueToColumnType = (value, column) => {
-    switch (column) {
-      case "Grade":
-        return isNaN(value) ? value : parseFloat(value);
-      // Add other cases for additional columns
-      default:
-        return value;
-    }
-  };
-
-  const handleColumnChange = (fixedColumn, value) => {
-    setColumnMappings((prevMappings) => ({
-      ...prevMappings,
-      [fixedColumn]: value,
-    }));
-  };
+  const [filesData, setFilesData] = useState([]);
 
   const handleFileUpload = (e) => {
-    const reader = new FileReader();
-    reader.readAsBinaryString(e.target.files[0]);
-    reader.onload = (e) => {
-      const data = e.target.result;
-      const workbook = XLSX.read(data, { type: "binary" });
-      const sheetName = workbook.SheetNames[0];
-      const sheet = workbook.Sheets[sheetName];
-      const parsedData = XLSX.utils.sheet_to_json(sheet);
-      console.log(parsedData);
-      setData(parsedData);
+    const files = Array.from(e.target.files);
+    const newFilesData = [];
 
-      // Initialize columnMappings with an empty mapping for each fixed column
-      const initialMappings = {};
-      fixedColumns.forEach((column) => {
-        initialMappings[column] = "";
-      });
-      setColumnMappings(initialMappings);
-    };
-  };
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.readAsBinaryString(file);
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "binary" });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const parsedData = XLSX.utils.sheet_to_json(sheet);
+        const newData = parsedData.slice(4, parsedData.length - 1);
+        const transformedArray = transformAndRenameArray(newData);
 
-  const handleAddFixedColumn = () => {
-    setFixedColumns((prevColumns) => [...prevColumns, newFixedColumn]);
-    setColumnMappings((prevMappings) => ({
-      ...prevMappings,
-      [newFixedColumn]: "",
-    }));
-    setNewFixedColumn("");
-  };
+        newFilesData.push({
+          fileName: file.name.split('.').slice(0, -1).join('.'),
+          data: transformedArray,
+        });
 
-  const isValidDate = (dateString) => {
-    if (!dateString) {
-      return false;
-    }
-    if (typeof dateString === "number") return false;
-
-    const [day, month, year] = dateString.split("/");
-    const date = new Date(`${year}-${month}-${day}`);
-    return !isNaN(date.getTime());
-  };
-
-  const validateCellType = (value, column) => {
-    switch (column) {
-      case "First Name":
-      case "Country":
-      case "Password":
-      case "Address":
-      case "Stop Name":
-        return typeof value === "string" ? "ok" : "error";
-      case "Date of Birth":
-        return isValidDate(value) ? "ok" : "error";
-      case "Grade":
-        return typeof value === "number" ? "ok" : "error";
-      case "Date of Birth":
-        return isValidDate(value) ? "ok" : "error";
-      case "Email":
-        // Basic email validation
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(value) ? "ok" : "error";
-      // Add other cases for additional columns
-      default:
-        return "ok";
-    }
-  };
-
-  const handleLogOverallData = () => {
-    console.log("Overall Data:", mappedData);
-  };
-
-  const handleInputValueChange = (rowIndex, column, value) => {
-    setMappedData((prevMappedData) => {
-      const updatedData = [...prevMappedData];
-      const updatedRow = { ...updatedData[rowIndex], [column]: value };
-
-      // Check the type validity of the value
-      const typeValidity = validateCellType(value, column);
-
-      // Update the row with typeValidity information
-      updatedRow.isValidType = typeValidity === "ok";
-      updatedRow.typeValidationError = typeValidity === "error";
-
-      updatedData[rowIndex] = updatedRow;
-      return updatedData;
+        if (newFilesData.length === files.length) {
+          setFilesData(newFilesData);
+        }
+      };
     });
   };
 
-  useEffect(() => {
-    const updatedMappedData = data.map((row) => {
-      const rowData = {};
-      fixedColumns.forEach((fixedColumn) => {
-        rowData[fixedColumn] = row[columnMappings[fixedColumn]];
-      });
-      return rowData;
+  const transformAndRenameArray = (arr) => {
+    const filteredArray = arr.filter((item) => item.hasOwnProperty("__EMPTY"));
+
+    const transformedArray = filteredArray.map((item) => {
+      const {
+        "Lokmanya Tilak College of Engineering, Navi Mumbai": SR_no,
+        __EMPTY: erpID,
+        __EMPTY_1: fullName,
+        __EMPTY_2: gender,
+        __EMPTY_3: DOB,
+        ...rest
+      } = item;
+
+      return { SR_no, erpID, fullName, gender, DOB };
     });
 
-    setMappedData(updatedMappedData);
-  }, [columnMappings, data, fixedColumns]);
-
-  useEffect(() => {
-    // Load data when selectedRowIndex changes
-    if (mappedData.length > 0) {
-      const selectedRow = mappedData[selectedRowIndex];
-      console.log("Selected Row Data:", selectedRow);
-      // Perform additional actions or data loading based on the selected row
-    }
-  }, [selectedRowIndex, mappedData]);
-
-  // const handleCellClick = (rowIndex, columnName, value) => {
-  //   setEditedCell({ rowIndex, columnName });
-  //   setEditedValue(value);
-  //   setIsEditing(true);
-  // };
-
-  // const handleInputBlur = () => {
-  //   setIsEditing(false);
-
-  //   // Update the data with the edited value
-  //   if (editedCell.columnName && editedValue !== "") {
-  //     handleInputValueChange(
-  //       editedCell.rowIndex,
-  //       editedCell.columnName,
-  //       editedValue
-  //     );
-  //   }
-  // };
-
-  // const handleColumnChange = (fixedColumn, value) => {
-  //   setColumnMappings((prevMappings) => ({
-  //     ...prevMappings,
-  //     [fixedColumn]: value,
-  //   }));
-  // };
-
-  // const handleFileUpload = (e) => {
-  //   const reader = new FileReader();
-  //   reader.readAsBinaryString(e.target.files[0]);
-  //   reader.onload = (e) => {
-  //     const data = e.target.result;
-  //     const workbook = XLSX.read(data, { type: "binary" });
-  //     const sheetName = workbook.SheetNames[0];
-  //     const sheet = workbook.Sheets[sheetName];
-  //     const parsedData = XLSX.utils.sheet_to_json(sheet);
-  //     console.log(parsedData);
-  //     setData(parsedData);
-
-  //     // Initialize columnMappings with an empty mapping for each fixed column
-  //     const initialMappings = {};
-  //     fixedColumns.forEach((column) => {
-  //       initialMappings[column] = "";
-  //     });
-  //     setColumnMappings(initialMappings);
-  //   };
-  // };
-
-  const handleNewFixedColumnChange = (e) => {
-    setNewFixedColumn(e.target.value);
+    return transformedArray;
   };
 
-  // const handleAddFixedColumn = () => {
-  //   setFixedColumns((prevColumns) => [...prevColumns, newFixedColumn]);
-  //   setColumnMappings((prevMappings) => ({
-  //     ...prevMappings,
-  //     [newFixedColumn]: "",
-  //   }));
-  //   setNewFixedColumn("");
-  // };
+  const handleDownloadZIP = async () => {
+    const zip = new JSZip();
 
-  const handleRowSelectionChange = (index) => {
-    setSelectedRowIndex(index);
+    filesData.forEach((fileData) => {
+      const jsonString = JSON.stringify(fileData.data, null, 2);
+      zip.file(`${fileData.fileName}.json`, jsonString);
+    });
+
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "files.zip");
   };
-
-  // const handleLogOverallData = () => {
-  //   console.log("Overall Data:", mappedData);
-  // };
-
-  const handleColumnMappingChange = (fixedColumn, value) => {
-    setColumnMappings((prevMappings) => ({
-      ...prevMappings,
-      [fixedColumn]: value,
-    }));
-  };
-
-  // const handleInputValueChange = (rowIndex, column, value) => {
-  //   setMappedData((prevMappedData) => {
-  //     const updatedData = [...prevMappedData];
-  //     const updatedRow = { ...updatedData[rowIndex], [column]: value };
-  //     updatedData[rowIndex] = updatedRow;
-  //     return updatedData;
-  //   });
-  // };
 
   return (
     <div className="h-screen w-screen py-16 px-16">
@@ -280,123 +75,16 @@ const Home = () => {
           type="file"
           accept=".xlsx, .xls"
           onChange={handleFileUpload}
+          multiple
           className="w-auto"
         />
-        <div>
-          {" "}
-          {data.length > 0 && (
-            <div className="flex items-center justify-center gap-2">
-              <Input
-                type="text"
-                value={newFixedColumn}
-                onChange={(e) => setNewFixedColumn(e.target.value)}
-              />
-
-              <Button type="button" onClick={handleAddFixedColumn}>
-                Add
-              </Button>
-            </div>
-          )}
-        </div>
       </div>
 
-      {data.length > 0 && (
+      {filesData.length > 0 && (
         <div>
           <h3>Map Columns:</h3>
-
-          <form>
-            <table className="table">
-              <thead>
-                <tr>
-                  {fixedColumns.map((fixedColumn) => (
-                    <th key={fixedColumn}>{fixedColumn}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  {fixedColumns.map((fixedColumn) => (
-                    <td key={fixedColumn}>
-                      <Select
-                        value={columnMappings[fixedColumn] || ""}
-                        onValueChange={(value) =>
-                          handleColumnMappingChange(fixedColumn, value)
-                        }
-                      >
-                        <SelectTrigger>
-                          <span>
-                            {columnMappings[fixedColumn] ||
-                              "-- Select Column --"}
-                          </span>
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Columns</SelectLabel>
-                            {Object.keys(data[0]).map((column) => (
-                              <SelectItem key={column} value={column}>
-                                {column}
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                    </td>
-                  ))}
-                </tr>
-
-                {mappedData.map((row, rowIndex) => (
-                  <tr
-                    key={rowIndex}
-                    className={
-                      rowIndex === selectedRowIndex ? "selected-row" : ""
-                    }
-                  >
-                    {fixedColumns.map((column) => (
-                      <td
-                        key={column}
-                        style={{
-                          backgroundColor:
-                            validateCellType(row[column], column) === "error"
-                              ? "red"
-                              : "white",
-                        }}
-                      >
-                        {isEditing &&
-                        editedCell.rowIndex === rowIndex &&
-                        editedCell.columnName === column ? (
-                          <Input
-                            type="text"
-                            value={editedValue}
-                            onChange={(e) => setEditedValue(e.target.value)}
-                            onBlur={handleInputBlur}
-                            autoFocus
-                            style={{
-                              backgroundColor:
-                                validateCellType(editedValue, column) ===
-                                "error"
-                                  ? "red"
-                                  : "white",
-                            }}
-                          />
-                        ) : (
-                          <div
-                            onClick={() =>
-                              handleCellClick(rowIndex, column, row[column])
-                            }
-                          >
-                            {row[column]}
-                          </div>
-                        )}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </form>
-
-          <Button type="button" onClick={handleLogOverallData}>
-            Log Overall Data
+          <Button type="button" onClick={handleDownloadZIP}>
+            Download ZIP
           </Button>
         </div>
       )}
---
+++ src/demo.js
@@ -0,0 +1,9 @@
+[
+  {
+    "Lokmanya Tilak College of Engineering, Navi Mumbai": 1,
+    __EMPTY: 230600205,
+    __EMPTY_1: "ANANYA VISHAL MAKKAR",
+    __EMPTY_2: "Female",
+    __EMPTY_3: "13-01-2006",
+  },
+];
